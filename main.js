8037875ae5f4c9a76b91d9f8d49439d56a66ca3c
Edu58 <edumuriithi58@gmail.com>
2022-04-05 09:23:03 +0300

    changes innerText types

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const countDown = () => {
-  const christmas = new Date("December 25, 2021 00:00:00").getTime();
+  const christmas = new Date("December 25, 2022 00:00:00").getTime();
   const now = new Date().getTime();
   const gap = christmas - now;
+
   const days = document.getElementById("days");
   const hours = document.getElementById("hours");
   const minutes = document.getElementById("minutes");
@@ -14,10 +15,10 @@ const countDown = () => {
   const hour = min * 60;
   const day = hour * 24;
 
-  days.innerText = Math.floor(gap / day);
-  hours.innerText = Math.floor((gap % day) / hour);
-  minutes.innerText = Math.floor((gap % hour) / min);
-  seconds.innerText = Math.floor((gap % min) / sec);
+  days.innerText = String(Math.floor(gap / day));
+  hours.innerText = String(Math.floor((gap % day) / hour));
+  minutes.innerText = String(Math.floor((gap % hour) / min));
+  seconds.innerText = String(Math.floor((gap % min) / sec));
 };
 
 setInterval(countDown, 1000);
